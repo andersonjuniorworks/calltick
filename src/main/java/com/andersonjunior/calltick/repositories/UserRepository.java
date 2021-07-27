d6d05fcaedec2c2a4aa1833479cf011e4a32f69e
andersonjuniorworks <andersonjunior.tech@gmail.com>
2021-07-27 13:17:31 -0300

    feature: criando Repository de usuário

---
+++ src/main/java/com/andersonjunior/calltick/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.andersonjunior.calltick.repositories;
+
+import java.util.List;
+
+import com.andersonjunior.calltick.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    List<User> findByFullname(String fullname);
+    
+    User findByEmail(String email);
+    
+}
