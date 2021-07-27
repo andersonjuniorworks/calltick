49fb22588417732bd05ac883bc347fd28a8cafd6
andersonjuniorworks <andersonjunior.tech@gmail.com>
2021-07-27 15:11:11 -0300

    feature: criando Repository de setor

---
+++ src/main/java/com/andersonjunior/calltick/repositories/SectorRepository.java
@@ -0,0 +1,15 @@
+package com.andersonjunior.calltick.repositories;
+
+import java.util.List;
+
+import com.andersonjunior.calltick.models.Sector;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SectorRepository extends JpaRepository<Sector, Integer>{
+    
+    List<Sector> findByDescription(String description);
+
+}
