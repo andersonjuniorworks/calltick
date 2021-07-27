c5b6de339bb934f194ddd76c31f1dc4881db8626
andersonjuniorworks <andersonjunior.tech@gmail.com>
2021-07-27 14:41:21 -0300

    feature: criando Repository de cliente

---
+++ src/main/java/com/andersonjunior/calltick/repositories/ClientRepository.java
@@ -0,0 +1,19 @@
+package com.andersonjunior.calltick.repositories;
+
+import java.util.List;
+
+import com.andersonjunior.calltick.models.Client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+    Client findByCpfOrCnpj(String cpfOrCnpj);
+
+    List<Client> findByFullname(String fullname);
+
+    List<Client> findByNickname(String nickname);
+
+}
