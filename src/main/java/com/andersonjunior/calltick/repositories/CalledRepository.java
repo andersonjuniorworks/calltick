2ebc791a66630652c44c44cb4fb543b43f736fa9
andersonjuniorworks <andersonjunior.tech@gmail.com>
2021-07-27 15:20:33 -0300

    feature: criando Repository de cliente

---
+++ src/main/java/com/andersonjunior/calltick/repositories/CalledRepository.java
@@ -0,0 +1,22 @@
+package com.andersonjunior.calltick.repositories;
+
+import java.util.Date;
+import java.util.List;
+
+import com.andersonjunior.calltick.models.Called;
+import com.andersonjunior.calltick.models.Client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CalledRepository extends JpaRepository<Called, Integer>{
+    
+    @Query(value = "SELECT c FROM Called c WHERE c.client = :client AND c.status = :status")
+    List<Called> findByClient(Client client, Boolean status);
+
+    @Query(value = "SELECT c FROM Called c WHERE c.openingDate BETWEEN :startDate AND :endDate")
+    List<Called> findByPeriod(Date startDate, Date endDate);
+
+}
