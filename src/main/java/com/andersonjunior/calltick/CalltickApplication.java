b5c943f4724a3320f2f2fe72364a8de13c0103ea
andersonjuniorworks <andersonjunior.tech@gmail.com>
2021-07-27 11:29:54 -0300

    Criando estrutura do projeto

---
+++ src/main/java/com/andersonjunior/calltick/CalltickApplication.java
@@ -0,0 +1,13 @@
+package com.andersonjunior.calltick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalltickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalltickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersonjunior/calltick/CalltickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersonjunior.calltick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalltickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
