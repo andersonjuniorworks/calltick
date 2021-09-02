4a1d9069be9a007c2d26454f94ae20d176014ad7
andersonjuniorworks <andersonjunior.tech@gmail.com>
2021-09-02 16:09:15 -0300

    feature: inicio da geração de chave

---
+++ src/main/java/com/andersonjunior/calltick/models/Paid.java
@@ -0,0 +1,13 @@
+package com.andersonjunior.calltick.models;
+
+import java.util.Date;
+
+public class Paid {
+
+    private Long id;
+    private Client client;
+    private Date dateOfPayment;
+    private Double amountPaid;
+    private int status;
+    
+}
