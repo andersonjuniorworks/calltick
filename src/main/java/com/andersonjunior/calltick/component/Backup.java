1ef1b084a02ffad12804220491ed2ceb9674bbf7
andersonjunior.works <andersonjunior.tech@gmail.com>
2021-10-13 21:55:26 -0300

    feature: implementando backup automatico

---
+++ src/main/java/com/andersonjunior/calltick/component/Backup.java
@@ -0,0 +1,28 @@
+package com.andersonjunior.calltick.component;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class Backup extends QuartzJobBean {
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+    
+        String arquivo = "C:\\Calltick\\backups";
+        arquivo = arquivo + "\\calltickdb_" + new java.util.Date().getTime() + ".sql";
+        String caminho = "C:\\calltick\\utils\\mysqldump.exe";
+        try {
+          Runtime bck = Runtime.getRuntime();
+          bck.exec(caminho + " -v -v -v --host=localhost --user=root --password=printf@javadev --port=3306 --protocol=tcp --force --allow-keywords --compress  --add-drop-table --default-character-set=latin1 --hex-blob  --result-file=" + arquivo + " --databases calltickdb");
+        } catch (IOException e) {
+          System.out.println(e);
+        } 
+
+    }
+
+}
---
+++ src/main/java/com/andersonjunior/calltick/config/QuartzConfig.java
@@ -0,0 +1,32 @@
+package com.andersonjunior.calltick.config;
+
+import com.andersonjunior.calltick.component.Backup;
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+    
+    @Bean
+    public JobDetail teatQuartzDetail(){
+        return JobBuilder.newJob(Backup.class).withIdentity("testQuartz").storeDurably().build();
+    }
+
+    @Bean
+    public Trigger testQuartzTrigger(){
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(43200)  //Set the time period in seconds
+                .repeatForever();
+        return TriggerBuilder.newTrigger().forJob(teatQuartzDetail())
+                .withIdentity("testQuartz")
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+
+}
